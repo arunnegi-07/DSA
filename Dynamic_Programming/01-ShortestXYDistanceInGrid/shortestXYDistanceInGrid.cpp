5a87e933452c30131ecefc447f61ee81d98a2c7f
arunnegi-07 <reacharunnegi@gmail.com>
2023-04-05 22:40:43 +0530

    Added README.md file in DP Problem-01

--- Dynamic_Programming/01-ShortestXYDistanceInGrid/shortestXYDistanceInGrid.cpp
+++ Dynamic_Programming/01-ShortestXYDistanceInGrid/shortestXYDistanceInGrid.cpp
@@ -1,35 +1,3 @@
-/*
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Problem : Shortest XY distance in Grid
-
-Give a N*M grid of characters 'O', 'X', and 'Y'. Find the minimum Manhattan distance between a X and a Y.
-
-Manhattan Distance :
-| row_index_x - row_index_y | + | column_index_x - column_index_y |
-
-Example 1:                                                                   Example 2:
- 
-Input:                                                                       Input:
-N = 4, M = 4                                                                 N = 3, M = 3
-grid  = {{X, O, O, O}                                                        grid = {{X, X, O}
-         {O, Y, O, Y}                                                                {O, O, Y}
-         {X, X, O, O}                                                                {Y, O, O}}
-         {O, Y, O, O}} 
-Output:                                                                      Output :
-1                                                                            2
-Explanation:                                                                 Explanation:
-{{X, O, O, O}                                                                {{X, X, O}
-{O, Y, O, Y}                                                                  {O, O, Y}
-{X, X, O, O}                                                                  {Y, O, O}}
-{O, Y, O, O}}                                                                The shortest X-Y distance in the grid is 2.
-The shortest X-Y distance in the grid is 1.                                  One possible such X and Y are marked in bold 
-One possible such X and Y are marked in bold                                 in the above grid.
-in the above grid.
-
-Expected Time Complexity: O(N*M)
-Expected Auxiliary Space: O(N*M)
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                                                                                                                                                                      */
 #include <bits/stdc++.h>
 using namespace std;
 
