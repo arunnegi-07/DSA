dfa4e08e69033df5095d544a785dc11e833d6937
arunnegi-07 <reacharunnegi@gmail.com>
2023-08-03 00:08:11 +0530

    Added Graph Problem 04 Using BFS

---
+++ Graph/04-ShortestDistanceInAMaze/shortestDistanceInAMaze.cpp
@@ -0,0 +1,91 @@
+// Just a little variation of previous problem --> only instead of starting point as {0, 0}  it is now given in the problem.
+// Using Breadth First Search (BFS)
+// TC : O(N*M)    SC : O(N*M)
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int shortestPath(vector<vector<int>> &grid, pair<int, int> source, pair<int, int> destination) {
+    int N = grid.size(); // Number of rows in the grid
+    int M = grid[0].size(); // Number of columns in the grid
+
+    int sr = source.first; // Row index of the source cell
+    int sc = source.second; // Column index of the source cell
+
+    int X = destination.first; // Row index of the destination cell
+    int Y = destination.second; // Column index of the destination cell
+
+    if (grid[sr][sc] == 0) // If starting cell (source) contains 0, it's not possible to reach the target.
+        return -1;
+
+    vector<vector<int>> vis(N, vector<int>(M, 0)); // To keep track of visited cells
+    queue<pair<int, int>> q; // Queue for BFS
+    q.push({sr, sc}); // Add the source cell to the queue
+    vis[sr][sc] = 1; // Mark the starting cell as visited
+
+    int delrow[] = {-1, 0, 1, 0}; // Array to represent possible changes in rows (up, right, down, left)
+    int delcol[] = {0, 1, 0, -1}; // Array to represent possible changes in columns (up, right, down, left)
+    int steps = 0; // Initialize the number of steps taken to reach the target cell
+
+    while (!q.empty()) { // Start the BFS traversal
+        int size = q.size(); // Get the number of cells at the current level of BFS
+        while (size--) { // Process all cells at the current level of BFS
+            int row = q.front().first; // Get the row index of the current cell
+            int col = q.front().second; // Get the column index of the current cell
+            q.pop(); // Remove the current cell from the queue
+
+            if (row == X && col == Y)
+                return steps; // If we reach the target cell, return the number of steps taken
+
+            for (int i = 0; i < 4; i++) { // Check all four possible directions (up, right, down, left)
+                int nrow = row + delrow[i]; // Calculate the row index of the neighbor cell
+                int ncol = col + delcol[i]; // Calculate the column index of the neighbor cell
+
+                // Check if the neighbor cell is within the grid bounds, not visited before, and is reachable (contains 1)
+                if (nrow >= 0 && nrow < N && ncol >= 0 && ncol < M && !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
+                    vis[nrow][ncol] = 1; // Mark the neighbor cell as visited
+                    q.push({nrow, ncol}); // Add the neighbor cell to the queue for further exploration
+                }
+            }
+        }
+        steps++; // Increment steps for each level of BFS (each iteration represents one level in the graph)
+    }
+
+    return -1; // If the target cell is not reachable, return -1
+}
+
+
+int main() {
+    // Sample input 1
+    vector<vector<int>> grid1 = {
+        {1, 1, 0, 1, 1},
+        {1, 0, 1, 0, 1},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 1},
+        {1, 1, 1, 1, 1}
+    };
+    pair<int, int> source1 = make_pair(0, 0);
+    pair<int, int> destination1 = make_pair(4, 3);
+
+    int steps1 = shortestPath(grid1, source1, destination1);
+    if (steps1 != -1) {
+        cout << "Sample Input 1: Steps to reach the target cell: " << steps1 << endl;
+    } else {
+        cout << "Sample Input 1: Target cell is not reachable." << endl;
+    }
+
+    // Sample input 2
+    pair<int, int> destination2 = make_pair(2, 4);
+    int steps2 = shortestPath(grid1, source1, destination2);
+    if (steps2 != -1) {
+        cout << "Sample Input 2: Steps to reach the target cell: " << steps2 << endl;
+    } else {
+        cout << "Sample Input 2: Target cell is not reachable." << endl;
+    }
+
+    return 0;
+}
